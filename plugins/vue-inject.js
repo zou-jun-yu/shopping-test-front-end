19a66063b739e56ba0d887c45dfe5d199d3a01b3
zou-uoz <ghyzjyworld_7010@163.com>
2020-08-29 13:03:05 +0800

    pm2 deploy

--- plugins/vue-inject.js
+++ plugins/vue-inject.js
@@ -1,4 +1,8 @@
-import Vue from 'vue'
+import Vue from "vue";
 
-Vue.prototype.$imagesDir="http://47.115.157.0:5000/images/uploads/";
+Vue.prototype.$imagesDir =
+  process.env.NODE_ENV === "production"
+    ? "http://47.115.157.0/images/"
+    : "http://127.0.0.1:5000/images/uploads/";
+// Vue.prototype.$imagesDir="http://47.115.157.0:5000/images/uploads/";
 // Vue.prototype.$imagesDir="http://127.0.0.1:5000/images/uploads/";
