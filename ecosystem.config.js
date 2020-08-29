d59b60f49f1894eec97db80112bc3ec43d0f1db5
zou-uoz <ghyzjyworld_7010@163.com>
2020-08-29 16:32:18 +0800

    pm2 deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,13 @@
 module.exports = {
   apps: [
     {
-      name: "NuxtShopping",
+      name: "ShoppingFront",
       instances: 1,
       script: "./node_modules/nuxt/bin/nuxt.js",
       args: "start",
+      log_date_format:"YYYY-MM-DD HH:mm Z",
+      watch: true,
+      ignore_watch : ["node_modules", ".nuxt"],
       env: {
         COMMON_VARIABLE: "true"
       },
@@ -16,23 +19,13 @@ module.exports = {
 
   deploy: {
     production: {
-      // key: `${process.env.HOME}/.ssh/your-privite.key`,
       user: "root",
       host: "47.115.157.0",
       ref: "origin/master",
-      // ssh_options: "StrictHostKeyChecking=no",
       repo: "git@github.com:zou-uoz/shopping-test-front-end.git",
       path: "/root/shopping-test/shopping-test-front-end",
-      // port: 22        //ssh端口
-      // 安装后置任务
-      // "post-setup": "npm install",
-      // 部署后置任务
-      // "post-deploy": "npm run build",
-      // pre-deploy: git fetch --all //部署前执行
-      // "pre-deploy-local": "",
       "post-deploy":
         "npm install && npm run build && pm2 reload ecosystem.config.js --env production"
-      // "pre-setup": ""
     }
   }
 };
