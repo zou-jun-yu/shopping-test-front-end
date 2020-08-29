4cf8a3e9da0d396be1e97f073a276d0c9eea04b6
zou-uoz <ghyzjyworld_7010@163.com>
2020-08-29 20:15:10 +0800

    pm2 deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,8 +6,8 @@ module.exports = {
       script: "./node_modules/nuxt/bin/nuxt.js",
       args: "start",
       log_date_format:"YYYY-MM-DD HH:mm Z",
-      watch: true,
-      ignore_watch : ["node_modules", ".nuxt"],
+      // watch: true,
+      // ignore_watch : ["node_modules", ".nuxt"],
       env: {
         COMMON_VARIABLE: "true"
       },
@@ -25,7 +25,8 @@ module.exports = {
       repo: "git@github.com:zou-uoz/shopping-test-front-end.git",
       path: "/root/shopping-test/shopping-test-front-end",
       "post-deploy":
-        "npm install && npm run build && pm2 reload ecosystem.config.js --env production"
+        // "git pull && npm install && npm run build && pm2 reload ecosystem.config.js --env production"
+        "git pull && npm run build && pm2 reload ecosystem.config.js --env production"
     }
   }
 };
